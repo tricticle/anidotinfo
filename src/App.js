c41abd8fcad9bf144b3dde33e1df0498f5bc2ccb
tricticle <tricticle@gmail.com>
2023-10-09 21:09:30 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,112 +1,270 @@
-import React, { useState, useEffect } from 'react';
+// src/App.jsx
+import React, { useRef, useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import the CSS file at the top of your App.jsx
+import Hls from 'hls.js';
 
-const App = () => {
-  const [animeDetails, setAnimeDetails] = useState(null);
+function App() {
+  const [animeData, setAnimeData] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [selectedAnime, setSelectedAnime] = useState(null);
+  const [episodeUrls, setEpisodeUrls] = useState([]);
+  const [selectedQuality, setSelectedQuality] = useState('');
+  const [totalEpisodes, setTotalEpisodes] = useState(0);
   const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      // Make a request to the AniList API with the search query
+      const response = await axios.get(`${url}${searchQuery}`, {
+        params: {
+          page: 1,
+          perPage: 20,
+        },
+      });
+
+      const { results } = response.data;
+      console.log(response.data)
+
+      // Update the search results state
+      setSearchResults(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const url = 'https://api.consumet.org/meta/anilist/';
 
   useEffect(() => {
-    const fetchAnimeDetails = async () => {
-      if (searchQuery.trim() === '') return;
-
-      const query = `
-        query ($searchQuery: String) {
-          Page {
-            media(search: $searchQuery, type: ANIME) {
-              id
-              title {
-                romaji
-                english
-                native
-              }
-              description
-              genres
-              episodes
-              averageScore
-              startDate {
-                year
-                month
-                day
-              }
-              endDate {
-                year
-                month
-                day
-              }
-              coverImage {
-                medium
-              }
-            }
-          }
-        }
-      `;
-
-      const variables = {
-        searchQuery: searchQuery.trim(),
-      };
-
-      const url = 'https://graphql.anilist.co';
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-          query,
-          variables,
-        }),
-      };
-
+    const fetchData = async () => {
       try {
-        const response = await fetch(url, options);
-        const { data } = await response.json();
-        const searchResults = data.Page.media;
-        setAnimeDetails(searchResults[0]);
+        const response = await axios.get(`${url}trending`, {
+          params: {
+            page: 1,
+            perPage: 20,
+          },
+        });
+
+        const { results } = response.data;
+
+        // Process the data if needed
+        setAnimeData(results);
+        console.log(results);
       } catch (error) {
-        handleError(error);
+        console.error(error);
       }
     };
 
-    fetchAnimeDetails();
-  }, [searchQuery]);
+    fetchData();
+  }, []);
 
-  const handleError = (error) => {
-    alert('Error, check console');
-    console.error(error);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${url}popular`, {
+          params: {
+            page: 1,
+            perPage: 20,
+          },
+        });
+
+        const { results } = response.data;
+
+        // Process the data if needed
+        setPopular(results);
+        console.log(results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const toggleInset = () => {
+    setSelectedAnime(selectedAnime ? null : selectedAnime);
   };
 
-  const handleSearchInputChange = (e) => {
-    setSearchQuery(e.target.value);
+
+  const fetchEpisodesByTitle = async (title, episodeNumber) => {
+    try {
+      const formattedTitle = title.replace(/ /g, '-').toLowerCase() + `-episode-${episodeNumber}`;
+      const response = await axios.get(`https://api.consumet.org/meta/anilist/watch/${formattedTitle}`);
+      const episodes = response.data;
+      return episodes;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  const loadAnimeDetails = async (animeId) => {
+    try {
+      const response = await axios.get(`${url}info/${animeId}`, {
+        params: {
+          provider: 'gogoanime',
+        },
+      });
+      const data = response.data;
+      setSelectedAnime(data);
+  
+      // Fetch the total number of episodes for the selected anime
+      setTotalEpisodes(data.totalEpisodes);
+  
+      // Try loading episodes by anime title (English first)
+      let episodes = await fetchEpisodesByTitle(data.title.english, 1);
+  
+      // If loading by English title fails, try loading by romaji title
+      if (episodes.length === 0) {
+        episodes = await fetchEpisodesByTitle(data.title.romaji, 1);
+      }
+  
+      // If loading by romaji title fails, try loading by Japanese title
+      if (episodes.length === 0) {
+        episodes = await fetchEpisodesByTitle(data.title.japanese, 1);
+      }
+  
+      setEpisodeUrls(episodes.sources);
+      setSelectedQuality(episodes.sources[0].quality); // Set the default quality
+    } catch (error) {
+      console.error(error);
+    }
+  };  
+  
+  const selectEpisode = async (episodeNumber) => {
+    // Try selecting episodes by English title first
+    let episodes = await fetchEpisodesByTitle(selectedAnime.title.english, episodeNumber);
+  
+    // If selecting by English title fails, try selecting by romaji title
+    if (episodes.length === 0) {
+      episodes = await fetchEpisodesByTitle(selectedAnime.title.romaji, episodeNumber);
+    }
+  
+    setEpisodeUrls(episodes.sources);
+    setSelectedQuality(episodes.sources[0].quality);
+  };
+  
+
+  const VideoPlayer = ({ episodeUrl }) => {
+    const videoRef = useRef(null);
+  
+    useEffect(() => {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(episodeUrl);
+        hls.attachMedia(videoRef.current);
+      }
+    }, [episodeUrl]);
+  
+    return (
+      <video
+      ref={videoRef}
+      controls
+      poster={selectedAnime ? selectedAnime.image : ''}
+    >
+        Your browser does not support the video tag.
+      </video>
+    );
   };
 
   return (
-    <div className="container">
-      <div>
+    <div className='container'>
+      <h1>trending</h1>
+      <div className='search-box'>
         <input
-          type="text"
-          placeholder="Enter anime title"
+          type='text'
+          placeholder='Search for anime...'
           value={searchQuery}
-          onChange={handleSearchInputChange}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
+        <button onClick={handleSearch}>Search</button>
       </div>
-      {animeDetails ? (
-        <div className='art-grid'>
-          <h2>{animeDetails.title.romaji}</h2>
-          <img className='art' src={animeDetails.coverImage.medium} alt="Anime Cover" />
-          <div className='about-page'> <p dangerouslySetInnerHTML={{ __html: animeDetails.description }}></p></div>
-          <section className='data'>
-          <p>Genres: {animeDetails.genres ? animeDetails.genres.join(', ') : 'N/A'}</p>
-          <p>Episodes: {animeDetails.episodes}</p>
-          <p>Average Score: {animeDetails.averageScore}</p>
-          <p>Start Date: {animeDetails.startDate.year}/{animeDetails.startDate.month}/{animeDetails.startDate.day}</p>
-          <p>End Date: {animeDetails.endDate.year}/{animeDetails.endDate.month}/{animeDetails.endDate.day}</p>
-        </section>
+      <div className='search-results'>
+        <div className="anime-list">
+        {searchResults.map((anime) => (
+          <div
+            className='anime-card'
+            key={anime.id}
+            style={{ backgroundColor: anime.color }}
+            onClick={() => loadAnimeDetails(anime.id)}
+          >
+          <img src={anime.image} alt={anime.title.english} />
+          <div className='ani-detail'>
+              <h3>{anime.title.english}</h3>
+              <p>Type: {anime.type}</p>
+              <p>Rating: {anime.rating}</p>
+              <p>Release Date: {anime.releaseDate}</p>
+            </div>
+          </div>
+        ))}
         </div>
-      ) : (
-        <></>
-      )}
+      </div>
+      <div className='anime-list'>
+        {animeData.map((anime) => (
+          <div
+            className='anime-card'
+            key={anime.id}
+            style={{ backgroundColor: anime.color }}
+            onClick={() => loadAnimeDetails(anime.id)} // Load details on click
+          >
+            <img src={anime.image} alt={anime.title.english} />
+            <div className='ani-detail'>
+              <h3>{anime.title.english}</h3>
+              <p>Type: {anime.type}</p>
+              <p>Rating: {anime.rating}</p>
+              <p>Release Date: {anime.releaseDate}</p>
+            </div>
+          </div>
+        ))}
+        <div className={`inset ${selectedAnime ? 'show' : 'hide'}`}>
+          {selectedAnime && (
+            <div className='anime-episodes' style={{ backgroundImage: `url(${selectedAnime.cover})`, backgroundSize: 'cover' }}>
+              <button className="close-button" onClick={toggleInset}> <i className="fas fa-close"></i></button>
+                {selectedQuality && (
+                  <div className="video">
+                <VideoPlayer episodeUrl={episodeUrls.find((episode) => episode.quality === selectedQuality).url} />
+                  </div>
+                )}
+                <div className="ani-detail">
+                <h3>{selectedAnime.title.english}</h3>
+              <div dangerouslySetInnerHTML={{ __html: selectedAnime.description }} />
+              <p>Available Qualities:</p>
+                </div>
+              <div className='server-list'>
+                {episodeUrls.map((episode, index) => (
+                  <div className='server-btn' key={index}>
+                    <button onClick={() => setSelectedQuality(episode.quality)}>{episode.quality}</button>
+                  </div>
+                ))}
+              </div>
+              {totalEpisodes > 1 && (
+              <div className="episode-list">
+                    {[...Array(totalEpisodes)].map((_, index) => (
+                        <button onClick={() => selectEpisode(index + 1)}>Episode {index + 1}</button>
+                    ))}
+                </div>
+              )}
+              </div>
+          )}
+        </div>
+        <h1>popular</h1>
+        <div className='anime-list'>
+        {popular.map((anime) => (
+          <div className='anime-card' key={anime.id} style={{ backgroundColor: anime.color }} onClick={() => loadAnimeDetails(anime.id)}>
+            <img src={anime.image} alt={anime.title.english} />
+            <div className='ani-detail'>
+              <h3>{anime.title.english}</h3>
+              <p>Type: {anime.type}</p>
+              <p>Rating: {anime.rating}</p>
+              <p>Release Date: {anime.releaseDate}</p>
+          </div>
+          </div>
+        ))}
+                    </div>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
